db6c941145be342cf8b5ee736f6a9151dacd23db
thusy <cleverage6@gmail.com>
2022-07-27 22:37:39 +0200

    AppBar Widget WIP

--- lib/Application/common_widgets/AppBarWidget.dart
+++ lib/Application/common_widgets/AppBarWidget.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mobile_ecommerce/Application/components/sign_in_component.dart';
 import 'package:mobile_ecommerce/Application/components/language_selection_component.dart';
+import 'package:mobile_ecommerce/Application/usecases/sign_in_usecase.dart';
+import 'package:mobile_ecommerce/Domain/Repositories_abstractions/connected_user_repository.dart';
+import 'package:mobile_ecommerce/Domain/Repositories_abstractions/user_repository.dart';
+import 'package:mobile_ecommerce/Infrastructure/Repositories_implementations/connected_user_repository_sqflite_impl.dart';
+import 'package:mobile_ecommerce/Infrastructure/Repositories_implementations/user_repository_sqflite_impl.dart';
 
 PreferredSizeWidget appBarWidget(context) {
   return AppBar(
@@ -14,16 +19,17 @@ PreferredSizeWidget appBarWidget(context) {
       height: 40,
     ),
     actions: <Widget>[
-      IconButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SignInComponent()),
-          );
-        },
-        icon: const Icon(FontAwesomeIcons.user),
-        color: const Color(0xFF323232),
-      ),
+      // IconButton(
+      //   onPressed: () {
+      //     Navigator.push(
+      //       context,
+      //       MaterialPageRoute(builder: (context) => SignInComponent()),
+      //     );
+      //   },
+      //   icon: const Icon(FontAwesomeIcons.user),
+      //   color: const Color(0xFF323232),
+      // ),
+      displayAccountOrConnectionIcon(context),
       IconButton(
         onPressed: () {
           showDialog(
@@ -34,5 +40,41 @@ PreferredSizeWidget appBarWidget(context) {
         color: const Color(0xFF323232),
       ),
     ],
+  );
+}
+
+Widget displayAccountOrConnectionIcon(BuildContext context) {
+  UserRepository userRepository = UserRepositorySqfliteImpl();
+  ConnectedUserRepository connectedUserRepository =
+      ConnectedUserRepositorySqfliteImpl();
+  SignInUsecase signInUsecase =
+      SignInUsecase(userRepository, connectedUserRepository);
+
+  return FutureBuilder(
+    future: signInUsecase.checkIfUserConnected(),
+    builder: (context, AsyncSnapshot snapshot) {
+      switch (snapshot.connectionState) {
+        case ConnectionState.none:
+        case ConnectionState.waiting:
+        default:
+          if (snapshot.hasError)
+            return Text('Error: ${snapshot.error}');
+          else if (snapshot.data == true)
+            return IconButton(
+                onPressed: () {},
+                icon: const Icon(FontAwesomeIcons.userNinja),
+                color: const Color(0xFF323232));
+          else
+            return IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignInComponent()),
+                  );
+                },
+                icon: const Icon(FontAwesomeIcons.user),
+                color: const Color(0xFF323232));
+      }
+    },
   );
 }

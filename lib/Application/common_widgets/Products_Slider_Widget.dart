ca843c219aad0729b7a785a3d2c5a822477b75b5
thusy <cleverage6@gmail.com>
2022-09-10 12:20:54 +0200

    Products Slider Widget : display OK

--- lib/Application/common_widgets/Products_Slider_Widget.dart
+++ lib/Application/common_widgets/Products_Slider_Widget.dart
@@ -4,8 +4,11 @@ import 'package:mobile_ecommerce/Domain/Entity/product.dart';
 
 class Products_Slider_Widget extends StatefulWidget {
   List<Product> products;
+  String title;
 
-  Products_Slider_Widget({Key? key, required this.products}) : super(key: key);
+  Products_Slider_Widget(
+      {Key? key, required this.products, required this.title})
+      : super(key: key);
 
   @override
   _Products_Slider_Widget_State createState() =>
@@ -20,39 +23,81 @@ class _Products_Slider_Widget_State extends State<Products_Slider_Widget> {
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(viewportFraction: 0.4);
+    _pageController = PageController(viewportFraction: 0.5);
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height / 3,
-      child: PageView.builder(
-          itemCount: widget.products.length,
-          pageSnapping: true,
-          controller: _pageController,
-          onPageChanged: (page) {
-            setState(() {
-              activePage = page;
-            });
-          },
-          itemBuilder: (context, pagePosition) {
-            return Container(
-              margin: EdgeInsets.all(10),
-              child: Products_Grid_Tiles_Component(
-                name: widget.products[pagePosition].getTitle().length <= 30
-                    ? widget.products[pagePosition].getTitle()
-                    : widget.products[pagePosition]
-                            .getTitle()
-                            .substring(0, 20) +
-                        '...',
-                imageUrl: widget.products[pagePosition].getImageUrl(),
-                product: widget.products[pagePosition],
-                price: widget.products[pagePosition].getPrice().toString(),
-              ),
-            );
-          }),
+    return Expanded(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    widget.title,
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   PageTransition(
+                      //     type: PageTransitionType.fade,
+                      //     child: ProductList(),
+                      //   ),
+                      // );
+                    },
+                    child: Text(
+                      "View All",
+                      style: TextStyle(fontSize: 18.0, color: Colors.blue),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 3,
+            child: PageView.builder(
+                itemCount: widget.products.length,
+                pageSnapping: true,
+                padEnds: false,
+                controller: _pageController,
+                onPageChanged: (page) {
+                  setState(() {
+                    activePage = page;
+                  });
+                },
+                itemBuilder: (context, pagePosition) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    child: Products_Grid_Tiles_Component(
+                      name:
+                          widget.products[pagePosition].getTitle().length <= 30
+                              ? widget.products[pagePosition].getTitle()
+                              : widget.products[pagePosition]
+                                      .getTitle()
+                                      .substring(0, 20) +
+                                  '...',
+                      imageUrl: widget.products[pagePosition].getImageUrl(),
+                      product: widget.products[pagePosition],
+                      price:
+                          widget.products[pagePosition].getPrice().toString(),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/Application/screens/Home_Screen.dart
+++ lib/Application/screens/Home_Screen.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_ecommerce/Application/common_widgets/Category_Slider_Widget.dart';
 import 'package:mobile_ecommerce/Application/common_widgets/Circular_Progress_Widget.dart';
 import 'package:mobile_ecommerce/Application/common_widgets/Products_Slider_Widget.dart';
 import 'package:mobile_ecommerce/Application/common_widgets/Search_Widget.dart';
 import 'package:mobile_ecommerce/Application/common_widgets/Promo_Slider_Widget.dart';
-import 'package:mobile_ecommerce/Domain/Entity/product.dart';
 import 'package:mobile_ecommerce/Domain/Repositories_abstractions/product_repository.dart';
 import 'package:mobile_ecommerce/Infrastructure/Repositories_implementations/product_repository_sqflite_impl.dart';
-
-/** 
-import 'package:mobile_ecommerce/components/BrandHomePage.dart';
-import 'package:mobile_ecommerce/components/CategorySlider.dart';
-import 'package:mobile_ecommerce/common_widget/PopularMenu.dart';
-import 'package:mobile_ecommerce/common_widget/TopPromoSlider.dart';
-import 'package:mobile_ecommerce/components/ShopHomePage.dart';
-*/
 
 class Home_Screen extends StatefulWidget {
   @override
@@ -29,81 +21,141 @@ get_trending_products() async {
   return await trending_products;
 }
 
+get_girls_products() async {
+  ProductRepository productRepository = ProductRepostitorySqfliteImpl();
+
+  var trending_products =
+      await productRepository.retrieveProductsByTitle('girl');
+
+  return await trending_products;
+}
+
 class _Home_Screen_State extends State<Home_Screen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Search_Widget(),
-          Promo_Slider_Widget(),
-          //PopularMenu(),
-          FutureBuilder(
-            future: get_trending_products(),
-            builder: (context, AsyncSnapshot snapshot) {
-              var products = snapshot.data;
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                case ConnectionState.waiting:
-                  return Circular_Progress_Widget();
-                default:
-                  if (snapshot.hasError)
-                    return Text('Error: ${snapshot.error}');
-                  else
-                    return Products_Slider_Widget(products: products);
-              }
-            },
-          ),
-          SizedBox(
-            height: 10,
-            child: Container(
-              color: Color(0xFFf5f6f7),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white70,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Search_Widget(),
+                  Promo_Slider_Widget(),
+                  //PopularMenu(),
+                  SizedBox(
+                    height: 15,
+                    child: Container(
+                      color: Color(0xFFf5f6f7),
+                    ),
+                  ),
+                  FutureBuilder(
+                    future: get_trending_products(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      var products = snapshot.data;
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.none:
+                        case ConnectionState.waiting:
+                          return Circular_Progress_Widget();
+                        default:
+                          if (snapshot.hasError)
+                            return Text('Error: ${snapshot.error}');
+                          else
+                            return Products_Slider_Widget(
+                                products: products,
+                                title: 'Latest Trending : Gini & Jony');
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                    child: Container(
+                      color: Color(0xFFf5f6f7),
+                    ),
+                  ),
+                  Category_Slider_Widget(),
+                  SizedBox(
+                    height: 15,
+                    child: Container(
+                      color: Color(0xFFf5f6f7),
+                    ),
+                  ),
+                  FutureBuilder(
+                    future: get_girls_products(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      var products = snapshot.data;
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.none:
+                        case ConnectionState.waiting:
+                          return Circular_Progress_Widget();
+                        default:
+                          if (snapshot.hasError)
+                            return Text('Error: ${snapshot.error}');
+                          else
+                            return Products_Slider_Widget(
+                                products: products,
+                                title: 'Our Selection for Girls');
+                      }
+                    },
+                  ),
+
+                  // SizedBox(
+                  //   height: 10,
+                  //   child: Container(
+                  //     color: Color(0xFFf5f6f7),
+                  //   ),
+                  // ),
+                  // PreferredSize(
+                  //   preferredSize: Size.fromHeight(50.0),
+                  //   child: TabBar(
+                  //     labelColor: Colors.black,
+                  //     tabs: [
+                  //       Tab(
+                  //         text: 'Categories',
+                  //       ),
+                  //       Tab(
+                  //         text: 'Brands',
+                  //       ),
+                  //       Tab(
+                  //         text: 'Shops',
+                  //       )
+                  //     ], // list of tabs
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   height: 10,
+                  //   child: Container(
+                  //     color: Color(0xFFf5f6f7),
+                  //   ),
+                  // ),
+                  // Expanded(
+                  //   child: TabBarView(
+                  //     children: [
+                  //       Container(
+                  //         color: Colors.white24,
+                  //         //child: CategoryPage(slug: 'categories/'),
+                  //       ),
+                  //       Container(
+                  //         color: Colors.white24,
+                  //         //child: BrandHomePage(slug: 'brands/?limit=20&page=1'),
+                  //       ),
+                  //       Container(
+                  //         color: Colors.white24,
+                  //         //child: ShopHomePage(
+                  //         //  slug: 'custom/shops/?page=1&limit=15',
+                  //         //),
+                  //       ) // class name
+                  //     ],
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
-          ),
-          PreferredSize(
-            preferredSize: Size.fromHeight(50.0),
-            child: TabBar(
-              labelColor: Colors.black,
-              tabs: [
-                Tab(
-                  text: 'Categories',
-                ),
-                Tab(
-                  text: 'Brands',
-                ),
-                Tab(
-                  text: 'Shops',
-                )
-              ], // list of tabs
-            ),
-          ),
-          SizedBox(
-            height: 10,
-            child: Container(
-              color: Color(0xFFf5f6f7),
-            ),
-          ),
-          Expanded(
-            child: TabBarView(
-              children: [
-                Container(
-                  color: Colors.white24,
-                  //child: CategoryPage(slug: 'categories/'),
-                ),
-                Container(
-                  color: Colors.white24,
-                  //child: BrandHomePage(slug: 'brands/?limit=20&page=1'),
-                ),
-                Container(
-                  color: Colors.white24,
-                  //child: ShopHomePage(
-                  //  slug: 'custom/shops/?page=1&limit=15',
-                  //),
-                ) // class name
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

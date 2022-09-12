2807e3c152da9ffa3d74108d1f453ad9d636f490
thusy <cleverage6@gmail.com>
2022-09-12 15:06:34 +0200

    Products Search Screen : add filter icon

--- lib/Application/screens/Products_Search_Screen.dart
+++ lib/Application/screens/Products_Search_Screen.dart
@@ -52,36 +52,50 @@ class _Products_Search_Screen_State extends State<Products_Search_Screen> {
           Card(
             child: Padding(
               padding: EdgeInsets.all(10.0),
-              child: Theme(
-                child: TextField(
-                  controller: searchController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Theme(
+                      child: TextField(
+                        controller: searchController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          filled: true,
+                          prefixIcon: Icon(Icons.search),
+                          fillColor: Color(0xFFF2F4F5),
+                          hintStyle: new TextStyle(color: Colors.grey[600]),
+                          hintText: translate("label_search"),
+                        ),
+                        autofocus: false,
+                        onSubmitted: (value) async {
+                          setState(() {
+                            searchTerms = value;
+                          });
+                        },
+                      ),
+                      data: Theme.of(context).copyWith(
+                        primaryColor: Colors.grey[600],
                       ),
                     ),
-                    filled: true,
-                    prefixIcon: Icon(Icons.search),
-                    fillColor: Color(0xFFF2F4F5),
-                    hintStyle: new TextStyle(color: Colors.grey[600]),
-                    hintText: translate("label_search"),
                   ),
-                  autofocus: false,
-                  onSubmitted: (value) async {
-                    setState(() {
-                      searchTerms = value;
-                    });
-                  },
-                ),
-                data: Theme.of(context).copyWith(
-                  primaryColor: Colors.grey[600],
-                ),
+                  IconButton(
+                      tooltip: 'Sort',
+                      icon: const Icon(Icons.filter_list),
+                      onPressed: () {
+                        // _showBottomSheet();
+                      }),
+                ],
               ),
             ),
           ),
+
           if (searchTerms != null && searchTerms.isNotEmpty)
             FutureBuilder(
               future: findProducts(searchTerms),
@@ -107,7 +121,6 @@ class _Products_Search_Screen_State extends State<Products_Search_Screen> {
                           scrollDirection: Axis.vertical,
                           shrinkWrap: true,
                           crossAxisCount: 2,
-//    physics: NeverScrollableScrollPhysics(),
                           padding: EdgeInsets.all(1.0),
                           childAspectRatio: 8.0 / 12.0,
                           children: List<Widget>.generate(productsList.length,

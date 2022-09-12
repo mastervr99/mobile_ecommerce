9ee748202e453b979e8ea102a949568d8210fe4e
thusy <cleverage6@gmail.com>
2022-09-12 18:41:51 +0200

    Products Search Screen : filters bottom sheet display - WIP

--- lib/Application/screens/Products_Search_Screen.dart
+++ lib/Application/screens/Products_Search_Screen.dart
@@ -23,12 +23,178 @@ class Products_Search_Screen extends StatefulWidget {
       _Products_Search_Screen_State(searchTerms: searchTerms);
 }
 
-class _Products_Search_Screen_State extends State<Products_Search_Screen> {
+class _Products_Search_Screen_State extends State<Products_Search_Screen>
+    with SingleTickerProviderStateMixin {
+  String selectedPeriod = "";
+  String selectedCategory = "";
+  String selectedPrice = "";
+
+  List<String> timeFilter = [
+    'Brand',
+    'New',
+    'Latest',
+    'Trending',
+    'Discount',
+  ];
+
+  List<String> categoryFilter = [
+    'Skull Candy',
+    'Boat',
+    'JBL',
+    'Micromax',
+    'Seg',
+  ];
+
+  List<String> priceFilter = [
+    '\$50-200',
+    '\$200-400',
+    '\$400-800',
+    '\$800-1000',
+  ];
+
   var searchTerms;
 
   _Products_Search_Screen_State({this.searchTerms});
 
   TextEditingController searchController = TextEditingController();
+
+  Widget _filters_modal() {
+    return Container(
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.05),
+                offset: Offset(0, -3),
+                blurRadius: 10)
+          ],
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(24), topLeft: Radius.circular(24)),
+          color: Colors.white),
+      child: ListView(
+        physics: NeverScrollableScrollPhysics(),
+//          controller: _scrollController,
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Filters',
+                style: TextStyle(color: Colors.grey[300]),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 32.0, top: 16.0, bottom: 16.0),
+              child: Text(
+                'Sort By',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            child: ListView.builder(
+              itemBuilder: (_, index) => Center(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedPeriod = timeFilter[index];
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 4.0, horizontal: 20.0),
+                        decoration: selectedPeriod == timeFilter[index]
+                            ? BoxDecoration(
+                                color: Color(0xffFDB846),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(45)))
+                            : BoxDecoration(),
+                        child: Text(
+                          timeFilter[index],
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+              )),
+              itemCount: timeFilter.length,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Container(
+            height: 50,
+            child: ListView.builder(
+              itemBuilder: (_, index) => Center(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedCategory = categoryFilter[index];
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 4.0, horizontal: 20.0),
+                        decoration: selectedCategory == categoryFilter[index]
+                            ? BoxDecoration(
+                                color: Color(0xffFDB846),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(45)))
+                            : BoxDecoration(),
+                        child: Text(
+                          categoryFilter[index],
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+              )),
+              itemCount: categoryFilter.length,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Container(
+            height: 50,
+            child: ListView.builder(
+              itemBuilder: (_, index) => Center(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedPrice = priceFilter[index];
+                      });
+                    },
+                    child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 4.0, horizontal: 20.0),
+                        decoration: selectedPrice == priceFilter[index]
+                            ? BoxDecoration(
+                                color: Color(0xffFDB846),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(45)))
+                            : BoxDecoration(),
+                        child: Text(
+                          priceFilter[index],
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+              )),
+              itemCount: priceFilter.length,
+              scrollDirection: Axis.horizontal,
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +255,19 @@ class _Products_Search_Screen_State extends State<Products_Search_Screen> {
                       tooltip: 'Sort',
                       icon: const Icon(Icons.filter_list),
                       onPressed: () {
-                        // _showBottomSheet();
+                        showModalBottomSheet(
+                            // isScrollControlled: true,
+                            context: context,
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadiusDirectional.only(
+                                topEnd: Radius.circular(25),
+                                topStart: Radius.circular(25),
+                              ),
+                            ),
+                            builder: (BuildContext context) {
+                              return _filters_modal();
+                            });
                       }),
                 ],
               ),

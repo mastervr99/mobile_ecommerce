795942cbd18e3d833dcbe25ca7e4fc646ae401ff
thusy <web.dev418@gmail.com>
2023-11-05 12:59:51 +0100

    Create class Video_slider_screen

---
+++ lib/Application/screens/Video_Slider_Screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:math' as math;
+
+class Video_Slider_Screen extends StatefulWidget {
+  @override
+  _Video_Slider_Screen_State createState() => _Video_Slider_Screen_State();
+}
+
+class _Video_Slider_Screen_State extends State<Video_Slider_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
